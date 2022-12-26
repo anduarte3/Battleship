0b58a22574c6bf7bcc1fe518d4bcf9c695f317d4
FADEwt <diamonvhero@gmail.com>
2022-12-26 17:39:00 +0000

    First commit

---
+++ tests/battleship.test.js
@@ -0,0 +1,5 @@
+import Ship from '../battleship.js'
+
+test('Returns an object with the following properties: average, min, max, and length', () => {
+    expect(x([1,8,3,4,2,6])).toEqual({average: 4, min: 1, max: 8, length: 6});
+});
